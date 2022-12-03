e5d76c8cdb271a66d1be6ecb7027e72a8fb806c1
Eoin H <UltimateHurl@gmail.com>
2022-12-03 10:58:02 +0000

    feat: Add Day 3

---
+++ day3/part_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import itertools
+from aocd import get_data
+from dotenv import load_dotenv
+
+load_dotenv()
+
+data = get_data(day=3, year=2022)
+
+def letters_to_priority(common):
+    def letter_to_priority(letter):
+        if letter == letter.lower():
+            return ord(letter) - 96
+        return ord(letter) - (64 - 26)
+    return sum(map(letter_to_priority, common))
+
+def find_common_letter(compartments):
+    common = set(compartments[0])
+    for compart in compartments[1:]:
+        common = common.intersection(set(compart))
+    return list(common)
+
+def split_compartments(rucksack):
+    halfway = int(len(rucksack)/2)
+    return [rucksack[:halfway], rucksack[halfway:]]
+
+rucksacks = [line for line in data.splitlines()]
+compartments = map(split_compartments, rucksacks)
+letters = map(find_common_letter, compartments)
+priorities = map(letters_to_priority, letters)
+answer = sum(priorities)
+print(answer)
---
+++ day3/part_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import itertools
+from aocd import get_data
+from dotenv import load_dotenv
+
+load_dotenv()
+
+data = get_data(day=3, year=2022)
+
+def letters_to_priority(common):
+    def letter_to_priority(letter):
+        if letter == letter.lower():
+            return ord(letter) - 96
+        return ord(letter) - (64 - 26)
+    return sum(map(letter_to_priority, common))
+
+def find_common_letter(compartments):
+    common = set(compartments[0])
+    for compart in compartments[1:]:
+        common = common.intersection(set(compart))
+    return list(common)
+
+def group_elves(rucksacks):
+    for i in range(0, len(rucksacks), 3):
+        yield rucksacks[i:i+3]
+
+groups = group_elves([line for line in data.splitlines()])
+letters = map(find_common_letter, groups)
+priorities = map(letters_to_priority, letters)
+answer = sum(priorities)
+print(answer)
