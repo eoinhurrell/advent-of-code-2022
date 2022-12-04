290676259700409d248e2cc25f9866ec559b1309
Eoin H <eoin.hurrell@gmail.com>
2022-12-04 11:40:09 +0000

    feat: Add Day 4

---
+++ day4/part_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import itertools
+from aocd import get_data
+from dotenv import load_dotenv
+
+load_dotenv()
+
+data = get_data(day=4, year=2022)
+
+def fully_contains(first, second):
+    return second.issuperset(first) or first.issuperset(second)
+
+def make_set(text_range):
+    start, end = text_range.split("-")
+    sections = set(range(int(start),int(end)+1))
+    return sections
+
+def parse_sections(line):
+    elves = line.split(",")
+    return fully_contains(*list(map(make_set, elves)))
+
+print(sum([parse_sections(line) for line in data.splitlines()]))
---
+++ day4/part_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import itertools
+from aocd import get_data
+from dotenv import load_dotenv
+
+load_dotenv()
+
+data = get_data(day=4, year=2022)
+
+def any_overlap(first, second):
+    return not second.isdisjoint(first)
+
+def make_set(text_range):
+    start, end = text_range.split("-")
+    sections = set(range(int(start),int(end)+1))
+    return sections
+
+def parse_sections(line):
+    elves = line.split(",")
+    return any_overlap(*list(map(make_set, elves)))
+
+print(sum([parse_sections(line) for line in data.splitlines()]))
