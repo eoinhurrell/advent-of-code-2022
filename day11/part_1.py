8846a41aa98a2f177c9f5beb3514039d57206816
Eoin H <eoin.hurrell@gmail.com>
2022-12-12 20:15:53 +0000

    chore: Cleanup test data

--- day11/part_1.py
+++ day11/part_1.py
@@ -8,33 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 data = get_data(day=11, year=2022).splitlines()
-data = """Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
-
-Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
-
-Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
-
-Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1""".split("\n")
 
 def make_monkey(monkey_text):
     monkey = {
@@ -89,7 +62,6 @@ def simulate_round(monkeys):
                 monkeys[monkey["false"]]["items"].append(worry)
             pass
         monkey["items"] = []  # seems right
-
 
 
 monkeys = list(map(make_monkey, reduce(make_monkey_text, data, [[]])))
